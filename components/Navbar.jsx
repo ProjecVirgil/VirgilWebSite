fc09c811c481adfe8fc138acf0d34e804ad6bda3
Francesco è volevi <deadragon99@outlook.it>
2023-08-06 11:02:59 +0200

    Fix idention (#2)

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,50 +5,48 @@ import React, { useState, useEffect } from 'react';
 import styles from '../styles';
 import { navVariants } from '../utils/motion';
 
-
 const Navbar = () => {
-  
-  const [file, setFile] = useState("");
+  const [file, setFile] = useState('');
   useEffect(() => {
     // Verifica se stai eseguendo il codice lato client (browser)
     if (typeof window !== 'undefined') {
       if (window.innerWidth <= 768) {
-        setFile("/VirgilAppV1.0.1a.apk");
+        setFile('/VirgilAppV1.0.1a.apk');
       } else {
-        setFile("/VirgilAI.zip");
+        setFile('/VirgilAI.zip');
       }
     }
   }, []);
 
   return (
-  <motion.nav
-    variants={navVariants}
-    initial="hidden"
-    whileInView="show"
-    className={`${styles.xPaddings} py-8 relative`}
-  >
-    <div className="relative w-[50%] inset-0 gradient-01" />
-    <div
-      className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+    <motion.nav
+      variants={navVariants}
+      initial="hidden"
+      whileInView="show"
+      className={`${styles.xPaddings} py-8 relative`}
     >
-      <a href={file} download>
+      <div className="relative w-[50%] inset-0 gradient-01" />
+      <div
+        className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+      >
+        <a href={file} download>
+          <img
+            src="/download.svg"
+            alt="search"
+            className="w-[24px] h-[24px] object-contain"
+          />
+        </a>
+        <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
+          PROJECT VIRGIL
+        </h2>
         <img
-          src="/download.svg"
-          alt="search"
+          src="/menu.svg"
+          alt="menu"
           className="w-[24px] h-[24px] object-contain"
         />
-      </a>
-      <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
-        PROJECT VIRGIL
-      </h2>
-      <img
-        src="/menu.svg"
-        alt="menu"
-        className="w-[24px] h-[24px] object-contain"
-      />
-    </div>
-  </motion.nav>
-    );
-  };
+      </div>
+    </motion.nav>
+  );
+};
 
 export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { Footer, Navbar } from '../components';
 import { About, Explore, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
-
 const Home = () => (
   <div className="bg-primary-black overflow-hidden">
     <Navbar />
@@ -24,6 +23,5 @@ const Home = () => (
     <Footer />
   </div>
 );
-
 
 export default Home;
