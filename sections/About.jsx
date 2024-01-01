aafe540d0a5e018006faf72ac97439eb4c40819e
Retr0 <dragonfaiver@gmail.com>
2024-01-01 10:00:08 +0100

    feat: Add fade in animation to About section

--- sections/About.jsx
+++ sections/About.jsx
@@ -6,6 +6,7 @@ import { TypingText } from '../components';
 import styles from '../styles';
 import { fadeIn, staggerContainer } from '../utils/motion';
 
+
 const About = () => (
   <section className={`${styles.paddings} relative z-10`}>
     <div className="gradient-02 z-0" />
